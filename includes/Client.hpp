1b326caaa983e5a2a4b0a299a7232d995644191d
Lorenzo Iafigliola <liafigli@e4r12p3.42roma.it>
2022-06-17 11:47:16 +0200

    Client + Channel

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -14,6 +14,7 @@ class Client
     private:
         std::string nickname;
         std::string username;   // !!! Lo username e' statico !!!
+        std::string hostname;
         int fd;
         int id;
         int is_msg;      //Variabile che indica se lo user ha appena inviato un messaggio
